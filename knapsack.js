bebadc88802fb1294d3ac052fe8b96f1de5120fd
Chandra Pareek <pareekchandra10@gmail.com>
2022-04-23 14:55:54 +0530

    commit

--- knapsack.js
+++ knapsack.js
@@ -1,186 +1,283 @@
-var num_rows
-var knapsackCapacity
-
-function createTable() {
-    knapsackCapacity = document.getElementById('capacity').value;
-    num_rows = document.getElementById('rows').value;
-    var theader = '<table class="table table-bordered" id="table"> <tr><th scope="col">Items</th> <th scope="col">Profit</th> <th scope="col">Weight</th></tr>';
-    var tbody = '';
-
-    for (var i = 0; i < num_rows; i++) {
-
-        tbody += '<tbody><tr>';
-        tbody += '<td>';
-        tbody += 'Item ' + i
-        tbody += '</td>'
-        for (var j = 0; j < 2; j++) {
-            tbody += '<td>';
-            tbody += '<input type="number" class="form-control" placeholder="Value"/>'
-            tbody += '</td>'
+const cal = document.querySelector('.knap-cal');
+const btn = document.querySelector('.btn_table');
+const knap_table = document.querySelector('.knap_table');
+const result = document.querySelector('.knapresult');
+const result2 = document.querySelector('.res');
+const result1 = document.querySelector('.knapresult1');
+const up = document.querySelector('.upper');
+var q, n, gfg;
+let p = [];
+let w = [];
+let w1 = [];
+let pw = [];
+function sortList(a, b) {
+    for (var i = 0; i < a.length; i++) {
+        let max = 0;
+        for (var j = i; j < a.length; j++) {
+            if (a[j] > max) {
+                max = a[j];
+                x = j;
+            }
         }
-        tbody += '</tr></tbody>\n';
-    }
-    var tfooter = '</table>';
-    document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
-}
+        let temp;
+        temp = a[i];
+        a[i] = a[x];
+        a[x] = temp;
+        temp = b[i];
+        b[i] = b[x];
+        b[x] = temp;
 
-
-
-var kpResultantProfitId = document.getElementById("kpResultantProfit")
-var kpProfitId = document.getElementById("kpProfit")
-var kpWeightId = document.getElementById("kpWeight")
-var kpProfitWeightId = document.getElementById("kpProfitWeight")
-var kpResultantSolutionId = document.getElementById("kpResultantSolution")
-
-var kp01ResultantProfitId = document.getElementById("kp01ResultantProfit")
-var kp01ProfitId = document.getElementById("kp01Profit")
-var kp01WeightId = document.getElementById("kp01Weight")
-
-var weightValue, profitValue
-var profit = [];
-var weight = [];
-var profit_weight = []
-var tempList = []
-var resultantSolution = []
-var i, j, knapsackResultantProfit = 0;
-
-function generateResult() {
-
-    knapsackCapacity = document.getElementById('capacity').value;
-    num_rows = document.getElementById('rows').value;
-
-    knapsackResultantProfit = 0;
-    profit = [];
-    weight = [];
-    profit_weight = []
-    tempList = []
-
-    var resultClass = document.getElementsByClassName("result");
-    console.log(resultClass.length);
-
-    for (i = 0; i < resultClass.length; i++) {
-        resultClass[i].style.visibility = "visible";
-    }
-
-    var tableId = document.getElementById("table")
-    for (var i = 1; i <= num_rows; i++) {
-        profitValue = tableId.rows[i].cells[1].children[0].value;
-        profit.push(profitValue)
-        tempList.push(profitValue)
-        weightValue = tableId.rows[i].cells[2].children[0].value;
-        weight.push(weightValue)
-    }
-    knapsack01Algorithm()
-    sortLists()
-
-    console.log("profit = " + profit);
-    console.log("weight = " + weight);
-    console.log("profit/weight = " + profit_weight);
-    knapsackAlgorithm()
-    console.log(knapsackResultantProfit);
-
-}
-
-
-function sortLists() {
-
-    // to find profit/weight
-    for (i = 0; i < num_rows; i++) {
-        profit_weight[i] = (profit[i] / weight[i])
-    }
-    console.log(tempList);
-
-    // to sort profit/weight in decreasing order along with profit and weight list
-    var list = [];
-    for (i = 0; i < num_rows; i++)
-        list.push({ 'profit_weight': profit_weight[i], 'profit': profit[i], 'weight': weight[i] });
-
-
-    list.sort(function (a, b) {
-        return ((a.profit_weight > b.profit_weight) ? -1 : ((a.profit_weight == b.profit_weight) ? 0 : 1));
-    });
-
-    for (i = 0; i < num_rows; i++) {
-        profit_weight[i] = +(list[i].profit_weight).toFixed(3)
-        profit[i] = list[i].profit;
-        weight[i] = list[i].weight;
     }
 }
+function isInteger(a) {
+    let b = parseInt(a);
+    if ((a - b) > 0)
+        return 0;
+    return 1;
+}
+function table(n) {
 
-// 9    4   4   2.5     2.5     2   0.625       >profit/weight
-// 18   20  12  25      10      22  5           >profit
-// 2    5   3   10      4       11  8           >weight
-// 23
-
-// 20, 25, 10, 12, 5, 22, 1 
-
-// applying knapsack algorithm
-function knapsackAlgorithm() {
-
-    for (i = 0; i < num_rows; i++) {
-        if (weight[i] <= knapsackCapacity) {
-            knapsackCapacity -= weight[i]
-            knapsackResultantProfit += +profit[i]
-            tempList[tempList.indexOf(profit[i])] = 1
-        }
-        else if (knapsackCapacity != 0) {
-            knapsackResultantProfit = +knapsackResultantProfit + +(profit[i] * (knapsackCapacity / weight[i]))
-            tempList[tempList.indexOf(profit[i])] = knapsackCapacity + "/" + weight[i]
-            knapsackCapacity = 0
-        }
-        else {
-            tempList[tempList.indexOf(profit[i])] = 0
-        }
+    var s = "";
+    knap_table.innerHTML = ""
+    for (var i = 0; i < n; i++) {
+        s += `
+        <th scope="row">${i + 1}</th>
+        <td><input type="number" class="form-control form1" id="profit${i}" placeholder="value" value = "" aria-describedby="basic-addon3"></td>
+        <td><input type="number" class="form-control form1" id = "weight${i}" placeholder="value" value = "" aria-describedby="basic-addon3"></td>
+      </tr>`
     }
-
-    kpResultantProfitId.innerHTML = +knapsackResultantProfit.toFixed(3)
-    kpProfitId.innerHTML = profit
-    kpWeightId.innerHTML = weight
-    kpProfitWeightId.innerHTML = profit_weight
-    kpResultantSolutionId.innerHTML = tempList
+    knap_table.innerHTML += `<table id = "table" class="table table-bordered border-primary">
+    <thead>
+      <tr>
+        <th scope="col">Items</th>
+        <th scope="col">Profit</th>
+        <th scope="col">Weight</th>
+      </tr>
+    </thead>
+    ${s} </table>`
 }
 
-// applying knapsack 0/1 algorithm
-function knapsack01Algorithm() {
-    var knapsackTable = new Array(num_rows)
-    for (i = 0; i <= num_rows; i++) {
-
-        knapsackTable[i] = Array(knapsackCapacity)
-        for (j = 0; j <= knapsackCapacity; j++) {
-            knapsackTable[i][j] = 0
-        }
+function knapnonfrac(q, n) {
+    let str = "";
+    let str1 = "";
+    gfg = new Array(n + 1);
+    for (let i = 0; i < gfg.length; i++) {
+        gfg[i] = new Array(q + 1);
     }
-
-    var theader = '<table class="table table-bordered">';
-    var tbody = '';
-
-    for (i = 1; i <= num_rows; i++) {
-        for (j = 0; j <= knapsackCapacity; j++) {
-            if (weight[i - 1] <= j) {
-                knapsackTable[i][j] = (Math.max(knapsackTable[i - 1][j], +knapsackTable[i - 1][j - weight[i - 1]] + +profit[i - 1]));
-                tbody += '<td>';
-                tbody += knapsackTable[i][j];
-                tbody += '</td>'
+    for (let i = 0; i < n + 1; i++) {
+        for (let j = 0; j < q + 1; j++) {
+            if (i == 0 || j == 0)
+                gfg[i][j] = 0;
+            else if ((j - w1[i - 1]) >= 0) {
+                gfg[i][j] = Math.max((parseInt(p[i - 1]) + gfg[i - 1][j - w1[i - 1]]), gfg[i - 1][j]);
             }
             else {
-                knapsackTable[i][j] = knapsackTable[i - 1][j]
-                tbody += '<td>';
-                tbody += knapsackTable[i][j];
-                tbody += '</td>'
+                gfg[i][j] = gfg[i - 1][j];
             }
         }
-        tbody += '</tr></tbody>\n';
+        if (i < n - 1) {
+            str += `${p[i]}, `;
+            str1 += `${w1[i]}, `;
+        }
+        else if (i == n - 1) {
+            str += `${p[i]}`;
+            str1 += `${w1[i]}`;
+        }
     }
-
-    var tfooter = '</table>';
-    document.getElementById('knapsackTable').innerHTML = theader + tbody + tfooter;
-
-    console.log(knapsackTable);
-    console.log(knapsackTable[num_rows][knapsackCapacity]);
-
-
-    kp01ResultantProfitId.innerHTML = knapsackTable[num_rows][knapsackCapacity]
-    kp01ProfitId.innerHTML = profit
-    kp01WeightId.innerHTML = weight
-
+    result2.innerHTML += `<table class="tab-result table table-bordered border-primary">
+    <thead>
+    <tr>
+      <th scope="col">0-1 Knapsack Problem</th>
+      <th scope="col">Result</th>
+    </tr>
+  </thead>
+    <tbody>
+        <tr>
+            <th scope="row">Maximum Profit :</th>
+            <td>${gfg[n][q]}</td>
+        </tr>
+        <tr>
+            <th scope="row">Profit :</th>
+            <td>${str} </td>
+        </tr>
+        <tr>
+            <th scope="row">Weight :</th>
+            <td>${str1} </td>
+        </tr>
+    </tbody>
+</table>`;
+    let str01 = "";
+    let str011 = "";
+    let strc011;
+    for (let i = 0; i < n + 1; i++) {
+        strc011 = "";
+        for (let j = 0; j < q + 1; j++) {
+            if (i == 0 && j == 0)
+                str01 += `<th scope="col">Item/Weight</th><th scope="col">W0</th>`;
+            else if (i == 0)
+                str01 += `<th scope="col">W${j}</th>`;
+            if (j == 0)
+                strc011 += `<th scope="col">Item${i}</th> <td>${gfg[i][j]}</td>`
+            else {
+                strc011 += `<td>${gfg[i][j]}</td>`;
+            }
+        }
+        str011 += `<tr>${strc011}</tr>`;
+    }
+    result1.innerHTML = `<div class="heading"><h2>0-1 KnapSack Table</h2></div><table class="tab-result1 table table-bordered border-primary">
+    <thead>
+      <tr>
+        ${str01}
+      </tr>
+    </thead>
+    <tbody>${str011}</tbody> </table>`
 }
+function calculate(q, n) {
+    var str = "";
+    var str1 = "";
+    for (var i = 0; i < n; i++) {
+        pw.push(p[i] / w[i]);
+    }
+    for (let i = 0; i < n; i++)
+        w1.push(w[i]);
+    sortList(pw, w);
+    let max_profit = 0;
+    for (var i = 0; i < n; i++) {
+        if ((q - w[i]) > 0) {
+            max_profit += (pw[i] * w[i]);
+            q -= w[i];
+            if (str.length == 0) {
+                if (isInteger(pw[i]))
+                    str += pw[i];
+                else
+                    str += pw[i].toFixed(3);
+            }
+            else {
+                if (isInteger(pw[i]))
+                    str += ", " + pw[i];
+                else
+                    str += ", " + pw[i].toFixed(3);
+            }
+            if (str1.length == 0) {
+                if (isInteger(w[i]))
+                    str1 += w[i];
+                else
+                    str1 += w[i].toFixed(3);
+            }
+            else {
+                if (isInteger(w[i]))
+                    str1 += ", " + w[i];
+                else
+                    str1 += ", " + w[i].toFixed(3);
+            }
+        }
+        else {
+            max_profit += (pw[i] * q);
+            if (str.length == 0) {
+                if (isInteger(pw[i]))
+                    str += pw[i];
+                else
+                    str += pw[i].toFixed(3);
+            }
+            else {
+                if (isInteger(pw[i]))
+                    str += ", " + pw[i];
+                else
+                    str += ", " + pw[i].toFixed(3);
+            }
+            if (str1.length == 0) {
+                if (isInteger(q))
+                    str1 += q;
+                else
+                    str1 += q.toFixed(3);
+            }
+            else {
+                if (isInteger(q))
+                    str1 += ", " + q;
+                else
+                    str1 += ", " + q.toFixed(3);
+            }
+            if (!isInteger(max_profit)) {
+                max_profit = max_profit.toFixed(3);
+            }
+            break;
+        }
+    }
+    result.classList.toggle('resultnone');
+    result1.classList.toggle('resultnone');
+    result2.innerHTML = `<table class="tab-result table table-bordered border-primary">
+    <thead>
+      <tr>
+        <th scope="col">Fractional Knapsack</th>
+        <th scope="col">Result</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="col">Maximum Profit : </th>
+            <td>${max_profit}</td>
+        </tr>
+        <tr>
+            <th scope="col">Profit/Weight : </th>
+            <td>${str} </td>
+        </tr>
+        <tr>
+            <th scope="col">Weight : </th>
+            <td>${str1} </td>
+        </tr>
+    </tbody>
+</table>`;
+}
+function take_input(q, n) {
+    p = [], w = [];
+    for (var i = 0; i < n; i++) {
+        proid = "profit" + i;
+        wgtid = "weight" + i;
+        var c = document.getElementById(proid).value;
+        var d = document.getElementById(wgtid).value;
+        if (c)
+            p.push(c);
+        else {
+            alert("Enter all values");
+            break;
+        }
+        if (d > 0)
+            w.push(d);
+        else {
+            alert("Enter all values");
+            break;
+        }
+    }
+    if ((p.length == n) && (w.length == n)) {
+        calculate(q, n);
+        knapnonfrac(q, n);
+    }
+}
+btn.addEventListener('click', () => {
+    q = parseInt(document.getElementById('bagcapacity').value);
+    n = parseInt(document.getElementById('Quantity').value);
+    if (!result.classList.contains('resultnone')) {
+        result.classList.toggle('resultnone');
+        result1.classList.toggle('resultnone');
+    }
+    if (n > 0) {
+        table(n);
+        if (cal.classList.contains('btn-hidd'))
+            cal.classList.toggle('btn-hidd');
+        document.getElementById('knaptable').scrollIntoView();
+    }
+    else
+        alert("Enter valid input");
+    j = 1;
+});
+up.addEventListener('click', () => {
+    document.getElementById('up').scrollIntoView();
+})
+cal.addEventListener('click', () => {
+    if (!result.classList.contains('resultnone')) {
+        result.classList.toggle('resultnone');
+        result1.classList.toggle('resultnone');
+    }
+    take_input(q, n);
+    document.getElementById('result-kn').scrollIntoView();
+});
